74e6b10095d4fefb6164ef26a2547e39d1b40e3b
qetroux <lambrosopos@gmail.com>
2021-10-31 16:13:40 +0900

    add routing and view for post/

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post', views.post_index, name='post_index'),
 ]
--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+
+def post_index(request):
+    return render(request, 'blog/post/post_index.html')
