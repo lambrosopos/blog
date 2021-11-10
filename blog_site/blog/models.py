a07e7fa72ba742c18909193316f37fc15f7f9b43
qetroux <lambrosopos@gmail.com>
2021-11-10 23:46:14 +0900

    adding post and tag model classes

--- blog_site/blog/models.py
+++ blog_site/blog/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=256)
+    subtitle = models.CharField(max_length=128)
+    post_body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+class Tags(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128)
