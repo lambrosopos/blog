f07cdb3a1119a0867d52e536d53ee240f9c97bc5
qetroux <lambrosopos@gmail.com>
2021-11-10 23:50:34 +0900

    add repr methods

--- blog_site/blog/models.py
+++ blog_site/blog/models.py
@@ -9,7 +9,13 @@ class Post(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
+    def __repr__(self):
+        return f"Post <{self.uuid}>"
+
 
 class Tags(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return f"Tag <{self.uuid}>" 
