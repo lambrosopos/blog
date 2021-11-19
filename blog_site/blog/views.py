015d6f273584759aa99f022cd0ec18bbb50d9e4a
qetroux <lambrosopos@gmail.com>
2021-11-18 21:10:54 -0900

    changing post_index view to class based view

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
 
-def post_index(request):
-    return render(request, 'blog/post/post_index.html')
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post/post_index.html'
 
 def post_view(request, post_id):
     return render(request, 'blog/post/post_view.html')
