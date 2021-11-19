2693319eb45b45a668f2f0e23aa5ea634c65e2ec
qetroux <lambrosopos@gmail.com>
2021-11-19 23:03:38 +0900

    change url to classbased

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post', views.post_index, name='post_index'),
+    path('post', views.PostListView.as_view(), name='post_index'),
     path('post/create', views.post_create, name='post_create'),
     path('post/<post_id>', views.post_view, name='post_view'),
     path('post/<post_id>/edit', views.post_edit, name='post_edit'),
