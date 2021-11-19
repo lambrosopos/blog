915e2bbdc50b3ce03bbf96125d68d440aef97279
qetroux <lambrosopos@gmail.com>
2021-11-19 23:25:06 +0900

    change post lists and items to classbased views

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post', views.PostListView.as_view(), name='post_index'),
+    path('post', views.PostIndexListView.as_view(), name='post_index'),
     path('post/create', views.post_create, name='post_create'),
-    path('post/<post_id>', views.post_view, name='post_view'),
+    path('post/<int:pk>', views.PostItemDetailView.as_view(), name='post_view'),
     path('post/<post_id>/edit', views.post_edit, name='post_edit'),
 ]
--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
-from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post, Tag
 
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post/post_index.html'
+class PostIndexListView(ListView):
+    model               = Post
+    template_name       = 'blog/post/post_index.html'
+    context_object_name = 'post_list'
 
-def post_view(request, post_id):
-    return render(request, 'blog/post/post_view.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['tag_list'] = Tag.objects.all()
+        return context
+
+class PostItemDetailView(DetailView):
+    model               = Post
+    template_name       = 'blog/post/post_view.html'
+    context_object_name = 'post_item'
+
 
 def post_create(request):
     return render(request, 'blog/post/post_create.html')
