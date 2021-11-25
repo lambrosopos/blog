b629d70f0512359ed097450959fc16758dcf279d
qetroux <lambrosopos@gmail.com>
2021-11-24 22:12:51 -0900

    setting up test for Post

--- blog_site/blog/tests.py
+++ blog_site/blog/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from blog.models import Post, Tag
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.test_post_1 = {
+            "title":"Test Post title",
+            "subtitle":"Test Post Subtitle",
+            "body":"#Header 1"
+        }
+
+        Post.objects.create(
+            title=self.test_post_1.title,
+            subtitle=self.test_post_1.subtitle,
+            body=self.test_post_1.body
+        )
+
+    def test_post_has_appropriate_properties(self):
+        test_post = Post.objects.get(
+            title=self.test_post_1.title,
+            subtitle=self.test_post_1.subtitle,
+            body=self.test_post_1.body
+        )
+        self.assertIsNotNone(test_post.uuid)
+        self.assertIsNotNone(test_post.created_at)
+        self.assertIsNotNone(test_post.updated_at)
+
