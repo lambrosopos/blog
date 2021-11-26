40ea51626e95b235c0ded601e8936913739c2c3f
qetroux <lambrosopos@gmail.com>
2021-11-25 19:09:51 -0900

    add test db settings

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -85,8 +85,11 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
-        'PORT': os.getenv('DB_PORT', '5432')
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_db'
+        },
     }
 }
 
