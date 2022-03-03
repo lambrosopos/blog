6cca8585e0527948371d0f221a1e82a55cb958a0
lambrosopos <lambrosopos@gmail.com>
2022-03-03 21:43:14 +0900

    simple db search with plain text search

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post, Tag
+from .import models as m
 
-# Create your views here.
+
 def index(request):
     return render(request, 'blog/index.html')
 
+
 class PostIndexListView(ListView):
-    model               = Post
+    model               = m.Post
     template_name       = 'blog/post/post_index.html'
     context_object_name = 'post_list'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tag_list'] = Tag.objects.all()
+        context['tag_list'] = m.Tag.objects.all()
         return context
 
 
 class PostItemDetailView(DetailView):
-    model               = Post
+    model               = m.Post
     template_name       = 'blog/post/post_view.html'
     context_object_name = 'post'
 
 
 def search_results(request):
-    return render(request, 'blog/search_results.html')
+    search_term = request.GET.get('q', '').strip()
+
+    context = {}
+
+    if search_term:
+        query_results = m.Post.objects.filter(title__icontains=search_term)
+        context['posts'] = [_ for _ in query_results]
+
+
+    return render(request, 'blog/search_results.html', context=context)
